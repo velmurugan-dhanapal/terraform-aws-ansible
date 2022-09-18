9535a5feb0c7f40f79922fa28f58ebefe8540f6a
velmurugan-dhanapal <54856501+velmurugan-dhanapal@users.noreply.github.com>
2022-09-18 18:29:29 -0400

    Update server.tf

--- server.tf
+++ server.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-2"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-0568773882d492fc8"
+  ami = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name = "velmrnd-ec2-instance-key-pair"
 
@@ -14,4 +14,20 @@ resource "aws_instance" "myawsserver" {
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
   }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
