27324da31ea0d4336c36a2e7696dff08ab940d7e
velmurugan-dhanapal <54856501+velmurugan-dhanapal@users.noreply.github.com>
2022-09-18 17:27:19 -0400

    Update server.tf

--- server.tf
+++ server.tf
@@ -4,7 +4,7 @@ region = "us-east-2"
 resource "aws_instance" "myawsserver" {
   ami = "ami-077e31c4939f6a2f3"
   instance_type = "t2.micro"
-  key_name = "vel-import"
+  key_name = "velmrnd-ec2-instance-key-pair"
 
   tags = {
     Name = "vel-DevOps-server"
